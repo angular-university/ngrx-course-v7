1f7b7ae0777e09f3e9bf8d6b464e8666b1ab81f5
JHades <jhades.dev@gmail.com>
2018-03-29 16:36:21 +0200

    Angular Ngrx Course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppState} from "./reducers";
+import {select, Store} from "@ngrx/store";
+import {Observable} from "rxjs/Observable";
+import {isLoggedIn, isLoggedOut} from "./auth/auth.selectors";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
+
+    constructor(private store: Store<AppState>) {
+
+    }
+
+    ngOnInit() {
+
+        this.isLoggedIn$ = this.store.pipe(select(isLoggedIn));
+
+        this.isLoggedOut$ = this.store.pipe(select(isLoggedOut));
+    }
+
+
 }
