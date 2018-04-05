7b468fb60d587269708734f26b5339517d22b66c
Your Name <you@example.com>
2018-04-05 18:12:18 +0200

    Angular Ngrx Course

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,40 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import {User} from '../model/user.model';
+import {AuthActions, AuthActionTypes} from '../auth/auth.actions';
 
 
+type AuthState = {
+  loggedIn: boolean,
+  user: User
+}
+
+const initialAuthState :AuthState = {
+  loggedIn: false,
+  user: undefined
+}
 
 export interface AppState {
+  auth: AuthState
+}
 
+function authReducer(state:AuthState = initialAuthState, action):AuthState  {
+  switch (action.type) {
+
+    case AuthActionTypes.LoginAction:
+      return {
+        loggedIn: true,
+         user: action.payload.user
+      }
+    default:
+      return state;
+  }
 }
 
 export const reducers: ActionReducerMap<AppState> = {
+
+  auth: authReducer
 
 };
 
