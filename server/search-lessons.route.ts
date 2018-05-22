7826555adde7598fa7bf52f5cabcd2fc54a7aa98
JHades <jhades.dev@gmail.com>
2018-05-22 12:03:21 +0200

    Angular Ngrx Course

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -11,31 +11,45 @@ export function searchLessons(req: Request, res: Response) {
 
     console.log('Searching for lessons ...');
 
-    const queryParams = req.query;
+    /*
+    const error = (Math.random() >= 0.5);
 
-    const courseId = queryParams.courseId,
-          filter = queryParams.filter || '',
-          sortOrder = queryParams.sortOrder,
-          pageNumber = parseInt(queryParams.pageNumber) || 0,
-          pageSize = parseInt(queryParams.pageSize);
-
-    let lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
-
-    if (filter) {
-       lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
+    if (error) {
+        console.log("ERROR loading lessons!");
+        res.status(500).json({message: 'random error occurred.'});
     }
+    else {
+    */
 
-    if (sortOrder == "desc") {
-        lessons = lessons.reverse();
-    }
+        const queryParams = req.query;
 
-    const initialPos = pageNumber * pageSize;
+        const courseId = queryParams.courseId,
+            filter = queryParams.filter || '',
+            sortOrder = queryParams.sortOrder,
+            pageNumber = parseInt(queryParams.pageNumber) || 0,
+            pageSize = parseInt(queryParams.pageSize);
 
-    const lessonsPage = lessons.slice(initialPos, initialPos + pageSize);
+        let lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
 
-    setTimeout(() => {
-        res.status(200).json({payload: lessonsPage});
-    },1000);
+        if (filter) {
+            lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
+        }
+
+        if (sortOrder == "desc") {
+            lessons = lessons.reverse();
+        }
+
+        const initialPos = pageNumber * pageSize;
+
+        const lessonsPage = lessons.slice(initialPos, initialPos + pageSize);
+
+        setTimeout(() => {
+            res.status(200).json({payload: lessonsPage});
+        },1000);
+
+   // }
+
+
 
 
 }
