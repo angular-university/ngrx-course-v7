30a5558e4094499fed7c61b91925629b3c85376f
JHades <jhades.dev@gmail.com>
2018-05-30 10:40:50 +0200

    Angular Ngrx Course

--- src/app/courses/course.selectors.ts
+++ src/app/courses/course.selectors.ts
@@ -25,6 +25,22 @@ export const selectAllCourses = createSelector(
 
 );
 
+export const selectBeginnerCourses = createSelector(
+    selectAllCourses,
+    courses => courses.filter(course => course.category === 'BEGINNER')
+);
+
+
+export const selectAdvancedCourses = createSelector(
+    selectAllCourses,
+    courses => courses.filter(course => course.category === 'ADVANCED')
+);
+
+export const selectPromoTotal = createSelector(
+  selectAllCourses,
+    courses => courses.filter(course => course.promo).length
+);
+
 
 export const allCoursesLoaded = createSelector(
   selectCoursesState,
--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -5,7 +5,7 @@ import {filter, map, tap, withLatestFrom} from "rxjs/operators";
 import {CoursesService} from "../services/courses.service";
 import {AppState} from '../../reducers';
 import {select, Store} from '@ngrx/store';
-import {selectAllCourses} from '../course.selectors';
+import {selectAdvancedCourses, selectAllCourses, selectBeginnerCourses, selectPromoTotal} from '../course.selectors';
 import {AllCoursesRequested} from '../course.actions';
 @Component({
     selector: 'home',
@@ -28,22 +28,11 @@ export class HomeComponent implements OnInit {
 
         this.store.dispatch(new AllCoursesRequested());
 
-        const courses$ = this.store
-          .pipe(
-            select(selectAllCourses)
-          );
+        this.beginnerCourses$ = this.store.pipe(select(selectBeginnerCourses));
 
-        this.beginnerCourses$ = courses$.pipe(
-          map(courses => courses.filter(course => course.category === 'BEGINNER') )
-        );
+        this.advancedCourses$ = this.store.pipe(select(selectAdvancedCourses));
 
-        this.advancedCourses$ = courses$.pipe(
-            map(courses => courses.filter(course => course.category === 'ADVANCED') )
-        );
-
-        this.promoTotal$ = courses$.pipe(
-            map(courses => courses.filter(course => course.promo).length)
-        );
+        this.promoTotal$ = this.store.pipe(select(selectPromoTotal));
 
     }
 
