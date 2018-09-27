d06d8d5ecaad8431551c581009d8bbb503e4f0f1
Shrinivassab <shrinivassab@gmail.com>
2018-09-27 15:54:02 -0400

    Understanding Reducers - Writing our First Reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,18 +1,31 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { User } from '../model/user.model';
+import { AuthActionTypes } from '../auth/auth.actions';
+
+interface AuthState {
+  loggedIn: boolean;
+  user: User;
+}
 
 export interface AppState {
+  auth: AuthState;
+}
 
+function authReducer(state: AuthState, action): AuthState {
+  switch (action.type) {
+    case AuthActionTypes.LoginAction:
+      return {
+        loggedIn: true,
+        user: action.payload.user
+      };
+    default:
+      return state;
+  }
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-
+  auth: authReducer
 };
 
 
