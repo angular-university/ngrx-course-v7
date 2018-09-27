a972374cf63edf1947e97cedc14d43206752719b
Shrinivassab <shrinivassab@gmail.com>
2018-09-27 14:05:44 -0400

    Setting up Ngrx Store with Ngrx Schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { environment } from '../environments/environment';
 import {RouterStateSerializer, StoreRouterConnectingModule} from "@ngrx/router-store";
 
 import { EffectsModule } from '@ngrx/effects';
+import { reducers, metaReducers } from './reducers';
+import { environment } from '../../environments/environment';
 
 
 const routes: Routes = [
@@ -50,6 +52,8 @@ const routes: Routes = [
         MatListModule,
         MatToolbarModule,
         AuthModule.forRoot(),
+        StoreModule.forRoot(reducers, { metaReducers }),
+        !environment.production ? StoreDevtoolsModule.instrument() : [],
     ],
     providers: [],
     bootstrap: [AppComponent]
