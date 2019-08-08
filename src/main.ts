bd596249eb5a48d069a550fdd6e71e998a81813f
Andre Fortin <andre.v.fortin@gmail.com>
2019-08-08 14:39:37 -0400

    Enable gesture support for Angular Material with hammerjs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-
+import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
